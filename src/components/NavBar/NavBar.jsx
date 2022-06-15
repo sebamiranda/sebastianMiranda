9f195e24b6d4dcddf91d956bfa4468af1d6820bc
sebamiranda <carlos.seba.miranda@gmail.com>
2022-06-15 20:32:54 -0300

    Modificacion de NavBar Responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,42 +1,208 @@
+// import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import { Home, ShoppingCartRounded, SkateboardingSharp } from '@mui/icons-material';
+// import CartWidget from './CartWidget';
+
+// export default function NavBar({cantidad}) {
+
+
+
+//   return (
+    
+//       <AppBar position="fixed" sx={{ bgcolor: "green" }}>
+//         <Toolbar>
+//         <IconButton color="inherit">
+//             <SkateboardingSharp></SkateboardingSharp>
+//         </IconButton>
+//         <Button color="inherit">INICIO</Button>
+       
+//           <Typography  component="div" sx={{ flexGrow: 1 }}>
+//            PRODUCTOS
+//           </Typography>          
+//           <Typography  component="div" sx={{ flexGrow: 1 }}>
+//             OFERTAS
+//           </Typography>
+//           <Typography  component="div" sx={{ flexGrow: 1 }}>
+//             PEDIDOS
+//           </Typography>
+//           <Button color="inherit">LOGIN</Button>
+
+//           <CartWidget cantidad={cantidad} />
+
+//         </Toolbar>
+//       </AppBar>
+    
+//   );
+// }
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Home, ShoppingCartRounded, SkateboardingSharp } from '@mui/icons-material';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
 import CartWidget from './CartWidget';
 
-export default function NavBar({cantidad}) {
+const pages = ['Productos', 'Ofertas', 'Novedades'];
+const settings = ['Perfil', 'Logout'];
 
+const ResponsiveAppBar = ({cantidad}) => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    
-      <AppBar position="fixed" sx={{ bgcolor: "green" }}>
-        <Toolbar>
-        <IconButton color="inherit">
-            <SkateboardingSharp></SkateboardingSharp>
-        </IconButton>
-        <Button color="inherit">INICIO</Button>
-       
-          <Typography  component="div" sx={{ flexGrow: 1 }}>
-           PRODUCTOS
-          </Typography>          
-          <Typography  component="div" sx={{ flexGrow: 1 }}>
-            OFERTAS
+    <AppBar position="static" sx={{ bgcolor: "#4caf50" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <FingerprintIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'roboto',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            INICIO
           </Typography>
-          <Typography  component="div" sx={{ flexGrow: 1 }}>
-            PEDIDOS
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="roboto"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'roboto',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            INICIO
           </Typography>
-          <Button color="inherit">LOGIN</Button>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
           <CartWidget cantidad={cantidad} />
-
+          
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Configuraciones">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Toolbar>
-      </AppBar>
-    
+      </Container>
+    </AppBar>
   );
-}
+};
+export default ResponsiveAppBar;
