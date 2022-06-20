e57b49f5271367919219a2daf6a351f51a173088
sebamiranda <carlos.seba.miranda@gmail.com>
2022-06-19 22:02:17 -0300

    Agregado de ItemCounter(contador de productos)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
-import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar'
 
 
 function App() {
   let cantidad=15;
   let greeting='Proximamente nuestro catalogo de productos en linea...'
+  let initial=15;
+  let stock=15;
+
+  function onAdd(valor){
+    if(valor!=0){
+    alert("Usted agrego "+ valor + " productos al carrito de compras.")
+  }
+  }
 
   return (
     <div className="App">
@@ -14,6 +23,7 @@ function App() {
         
        <NavBar cantidad={cantidad}></NavBar>
        <ItemListContainer greeting={greeting}/>
+       <ItemCount stock ={stock} initial={initial} onAdd={onAdd}/>
        
       </header>
     </div>
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import Box from '@mui/material/Box';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { Grid } from '@mui/material';
+
+export default function ItemCount({stock, initial, onAdd}) {
+
+const [auxInitial, setAuxInitial]= useState(initial);
+
+function restar(){
+    if(auxInitial >= 1){
+        setAuxInitial(auxInitial - 1);
+    }
+}
+function sumar(){
+    if(auxInitial < stock){
+        setAuxInitial(auxInitial + 1);
+    }
+}
+
+
+
+  return (
+    <div>
+    <Card sx={{ maxWidth: 200 }}>
+      <CardMedia
+        component="img"
+        height="50"
+        image=""
+        alt=""
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Producto
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+         Descripcion
+        </Typography>
+      </CardContent>
+      <CardActions>
+      
+      <Grid container>
+          <Grid item xs={12}>
+          <Box sx={{ '& > :not(style)': { m: 1 } }}>
+              <Fab color="primary" size='small' onClick={()=>restar()}>
+                <HorizontalRuleIcon />
+              </Fab>
+              <Fab color="string" size='small'>
+              {auxInitial}
+              </Fab>
+              <Fab color="primary" size='small' onClick={()=>sumar()}>
+                <AddIcon />
+              </Fab>
+              </Box>
+          </Grid>
+          <Grid item xs={12} >    
+          <Box sx={{ '& > :not(style)': { m: 1 } }}> 
+            <Button variant="outlined" onClick={()=>onAdd(auxInitial)}>AGREGAR</Button>
+            </Box>
+          </Grid>
+      </Grid>
+      </CardActions>
+    </Card>
+    </div>
+  )
+}
